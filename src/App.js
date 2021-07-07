0298c0c3cb4ff59fddbb0727a9b99d672c2414a4
marianovatri <marianovatri@gmail.com>
2021-07-07 13:34:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+  const [count, setCount] = useState("");
+  
+  const [resultado] = useState(false);
+ 
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+     <div>      
+
+      <p>Calculadora</p>
+       <div>
+      
+       <p >{count} </p>
+
+      </div>
+      
+      <button onClick={() => setCount(count + "7" )}>
+        7
+      </button>
+
+
+      <button onClick={() => setCount(count + "8")}>
+        8
+      </button>
+      <button onClick={() => setCount(count + "9")}>
+        9
+      </button>
+      <button onClick={() => setCount(count + "/")}>
+        /
+      </button>
+
+      <div>
+
+      <button onClick={() => setCount(count + "4")}>
+        4
+      </button>
+      <button onClick={() => setCount(count + "5")}>
+        5
+      </button>
+      <button onClick={() => setCount(count + "6")}>
+        6
+      </button>
+      <button onClick={() => setCount(count + "x")}>
+        x
+      </button>
+
+      </div>
+      
+      <div>
+      <button onClick={() => setCount(count + "1")}>
+        1
+      </button>
+      <button onClick={() => setCount(count + "2")}>
+        2
+      </button>
+      <button onClick={() => setCount(count + "3")}>
+        3
+      </button>
+      <button onClick={() => setCount(count + "+")}>
+        +
+      </button>
+
+      </div>
+
+      <div>
+
+      <button onClick={() => setCount(count + ".")}>
+        .
+      </button>
+      <button onClick={() => setCount(count + "0")}>
+        0
+      </button>
+      <button onClick={() => setCount(count + "=")}>
+        =
+      </button>
+      <button onClick={() => setCount(count + "-")}>
+        -
+      </button>
+
+      </div>
+
+      
+
     </div>
+    
   );
+  
 }
 
-export default App;
+
+
+ 
+
+
+export default (App)
+
